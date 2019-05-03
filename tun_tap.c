6294ad1742a7b21bdb04f8ee02dda10961c7231a
merna177 <ronazakaria@gmail.com>
2019-05-03 10:46:05 +0200

    push frame parsing

--- tun_tap.c
+++ tun_tap.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <errno.h>
+
 /*commands used for creating tap device 
 sudo openvpn --mktun --dev tap1
 sudo ip addr add 10.0.0.1/24 dev tap1
@@ -17,6 +18,28 @@ and set it’s address to 10.0.0.1 If we run ip link we observe that the link is
 Since there is no program attached to this virtual interface 
 the kernel treats it like an Ethernet adapter without a cable connected (NO-CARRIER).
 */
+/*
+ethernet structure
+*/
+struct ethernet_header
+{
+    unsigned char dmac[6];
+    unsigned char smac[6];
+    uint16_t ethertype;
+    unsigned char payload[];
+} __attribute__((packed));
+/*
+arp header
+*/
+struct arp_header
+{
+    uint16_t hwtype;
+    uint16_t protype;
+    unsigned char hwsize;
+    unsigned char prosize;
+    uint16_t opcode;
+    unsigned char data[];
+} __attribute__((packed));
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 /*
 The TUN/TAP driver is a way for userspace applications to do networking. It creates a virtual network interface that behaves like a real one but every packet it receives gets forwarded to a userspace application. TUN adapters operate with layer 3 packets (such as IP packets) and TAP adapter works with layer 2 packets (like Ethernet frames). TUN is therefore useful for creating a point-to-point network tunnel while TAP can bridge two networks like a switch (but at a slightly higher overhead).
@@ -49,7 +72,7 @@ static int tun_alloc(char *dev)
 If a non-existent or empty interface name is specified, the kernel will create a new interface if the user has the CAP_NET_ADMIN permission (or is root).
 */
     if( (err = ioctl(file_descriptor, TUNSETIFF, (void *) &interface_data)) < 0 ){
-        perror("ERR: Could not ioctl tun");
+        perror("Error: Could not ioctl tun");
         close(file_descriptor);
         return err;
     }
@@ -72,6 +95,12 @@ int tun_write(int file_descriptor,char *buffer, int buffer_length)
 {
     return write(file_descriptor, buffer, buffer_length);
 }
+int check_type_of_network_layer(ethernet_header *hdr){
+   if(hdr->ethertype==ETH_P_ARP)
+     return 0;
+   else if(hdr-> ethertype == ETH_P_IP)
+     return 1;
+}
 int main(){
 
   char tun_name[IFNAMSIZ];
@@ -82,7 +111,6 @@ int main(){
    printf("error in Allocating interface\n");
    return 0;
   }
-
 while(1) {
   /* Now read data coming from the kernel */
   /* Note that "buffer" should be at least the MTU size of the interface, eg 1500 bytes */
@@ -93,8 +121,11 @@ while(1) {
     close(file_descriptor);
     return 0;
   }
+  //parsing ethernet package format 
   printf("Read %d bytes from device %s %s\n", read_value, tun_name,buffer);
- 
+  struct ethernet_header *hdr =  (struct ethernet_header *)(buffer);  
+  check_type_of_network_layer(hdr);
+
  }
   return 0;
 }
